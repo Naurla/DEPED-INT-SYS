ba2fa7406a91ae9058d005f1998927c510034b2a
Kimdokjer <trebeojyotnis@gmail.com>
2026-03-05 13:47:18 +0800

    font weight

--- deped_sys/resources/views/admin/advisories/index.blade.php
+++ deped_sys/resources/views/admin/advisories/index.blade.php
@@ -40,7 +40,7 @@
 
     <aside class="bg-[#b91c1c] text-white transition-all duration-300 flex flex-col shadow-xl" :class="sidebarOpen ? 'w-64' : 'w-20'">
         <div class="p-6 border-b border-red-800 flex items-center justify-between">
-            <h1 class="font-bold tracking-tighter overflow-hidden whitespace-nowrap" x-show="sidebarOpen">DEPED ADMIN</h1>
+            <h1 class="font-bold tracking-tighter text-lg whitespace-nowrap uppercase" x-show="sidebarOpen">DEPED ADMIN</h1>
             <button @click="sidebarOpen = !sidebarOpen" class="hover:bg-red-700 p-1 rounded">
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
             </button>
